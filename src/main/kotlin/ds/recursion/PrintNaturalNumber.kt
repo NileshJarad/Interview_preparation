d217426badd2ba51437818b607c2050d9fac50b2
Nilesh Jarad <nilworking@gmail.com>
2022-09-20 21:28:08 +0530

    Added amazon questions

---
+++ src/main/kotlin/ds/recursion/PrintNaturalNumber.kt
@@ -0,0 +1,31 @@
+package ds.recursion
+
+import sun.jvm.hotspot.runtime.DeadlockDetector.print
+import java.sql.DriverManager.println
+
+
+fun main() {
+    print("Print dec number from 10 : ")
+    PrintNaturalNumber().dec(10)
+    println()
+    print("Print inc number till 10 : ")
+    PrintNaturalNumber().inc(10)
+}
+
+class PrintNaturalNumber {
+    fun dec(n: Int) {
+        if (n == 0) {
+            return
+        }
+        print("$n ") // this is executed when we are moving towards the base case as it's before function call
+        dec(n - 1)
+    }
+
+    fun inc(n: Int) {
+        if (n == 0) {
+            return
+        }
+        inc(n - 1)
+        print("$n ") // this will execute after the function returns
+    }
+}
