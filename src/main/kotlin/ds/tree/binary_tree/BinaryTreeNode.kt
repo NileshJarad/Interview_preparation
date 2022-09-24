9105df55718d661ef5137654accf352119b2f12d
Nilesh Jarad <nilworking@gmail.com>
2022-09-24 20:50:49 +0530

    Added top view of the binary tree

--- src/main/kotlin/ds/tree/binary_tree/BinaryTreeNode.kt
+++ src/main/kotlin/ds/tree/binary_tree/BinaryTreeNode.kt
@@ -1,9 +1,43 @@
 package ds.tree.binary_tree
 
 import ds.tree.binary_search_tree.BinarySearchTree
+import java.util.*
 
 class BinaryTreeNode(
     var data: Int,
     var leftNode: BinaryTreeNode? = null,
     var rightNode: BinaryTreeNode? = null
 )
+
+fun BinaryTreeNode.levelOrder() {
+    val queue = LinkedList<BinaryTreeNode>()
+    queue.push(this)
+    while (!queue.isEmpty()) {
+        val pop = queue.pop()
+        print("${pop?.data} - ")
+        if (pop.leftNode != null) {
+            queue.add(pop.leftNode!!)
+        }
+        if (pop.rightNode != null) {
+            queue.add(pop.rightNode!!)
+        }
+    }
+}
+
+fun BinaryTreeNode.postOrderTraversal() {
+    this.leftNode?.postOrderTraversal()
+    this.rightNode?.postOrderTraversal()
+    print("$data - ")
+}
+
+fun BinaryTreeNode.inOrderTraversal() {
+    this.leftNode?.inOrderTraversal()
+    print("$data - ")
+    this.rightNode?.inOrderTraversal()
+}
+
+fun BinaryTreeNode.preOrderTraversal() {
+    print("$data - ")
+    this.leftNode?.preOrderTraversal()
+    this.rightNode?.preOrderTraversal()
+}
---
+++ src/main/kotlin/ds/tree/binary_tree/TopViewOfBinaryTree.kt
@@ -0,0 +1,71 @@
+package ds.tree.binary_tree
+
+import java.util.*
+
+
+fun main() {
+    // create binary tree
+    val root = BinaryTreeNode(1)
+    root.leftNode = BinaryTreeNode(2)
+    root.rightNode = BinaryTreeNode(3)
+    root.leftNode?.leftNode = BinaryTreeNode(4)
+    root.rightNode?.leftNode = BinaryTreeNode(5)
+    root.rightNode?.rightNode = BinaryTreeNode(6)
+    root.rightNode?.leftNode?.leftNode = BinaryTreeNode(7)
+    root.rightNode?.leftNode?.rightNode = BinaryTreeNode(8)
+    root.rightNode?.leftNode?.rightNode?.rightNode = BinaryTreeNode(9)
+    root.rightNode?.leftNode?.rightNode?.rightNode?.rightNode = BinaryTreeNode(10)
+
+//    root.levelOrder()
+    println()
+    root.preOrderTraversal()
+//    println()
+//    root.inOrderTraversal()
+    println()
+    root.postOrderTraversal()
+
+    println()
+    TopViewOfBinaryTree().topViewOfTree(root);
+}
+
+typealias  level = Int
+typealias  value = Int
+
+
+class TopViewOfBinaryTree {
+    fun topViewOfTree(root: BinaryTreeNode) {
+
+        val queue = LinkedList<Pair<BinaryTreeNode, level>>()
+        val map = TreeMap<level, value>()
+        queue.add(Pair(root, 0))
+        while (queue.isNotEmpty()) {
+            val (poppedNode, poppedLevel) = queue.pop()
+
+            if (map[poppedLevel] == null) {
+                map[poppedLevel] = poppedNode.data
+            }
+
+            if (poppedNode.leftNode != null) {
+                queue.add(Pair(poppedNode.leftNode!!, poppedLevel - 1))
+            }
+
+            if (poppedNode.rightNode != null) {
+                queue.add(Pair(poppedNode.rightNode!!, poppedLevel + 1))
+            }
+        }
+
+        map.forEach { _, value ->
+            print("${value} -")
+        }
+
+    }
+}
+
+/**
+ * What is reason to use TreeMap
+ *
+ * 1) TreeMap offers O(log N) lookup and insertion
+ * 2) Keys are ordered
+ * 3) It cannot have null key but can have multiple null values.
+ * 4) It is same as HashMap instead maintains ascending order(Sorted using the natural order of its key).
+ */
