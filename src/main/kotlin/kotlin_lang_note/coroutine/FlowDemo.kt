51fb1b3945e901543da7c984597f262e2dabd337
Nilesh Jarad <nilworking@gmail.com>
2022-11-01 10:00:03 +0530

    Flow operators

--- src/main/kotlin/kotlin_lang_note/coroutine/FlowDemo.kt
+++ src/main/kotlin/kotlin_lang_note/coroutine/FlowDemo.kt
@@ -8,25 +8,32 @@ import kotlinx.coroutines.flow.*
  */
 fun main() = runBlocking {
 
+    flowOnDemo()
 
-    /**
-     *
-     * Zip operator
-     * https://kotlinlang.org/api/kotlinx.coroutines/kotlinx-coroutines-core/kotlinx.coroutines.flow/zip.html
-     *
-     * Zips values from the current flow (this) with other flow using provided transform function applied to each pair of values.
-     * The resulting flow completes as soon as one of the flows completes and cancel is called on the remaining flow.
-     */
-    numFlow().zip(namesFlow()) { num, name -> "$num - $name" }.collect {
-        println("Zipped values = $it")
-    }
+//    zipOperator()
+//    withIndextedOperator()
+//    reduceOperator()
+//    foldOperator()
+}
 
+private suspend fun flowOnDemo() {
+    numFlow().map {
+        println("Map ${Thread.currentThread()}")
+        it * 3
+    }.onStart {
+        println("On Start ${Thread.currentThread()}")
+    }.flowOn(Dispatchers.IO) // upstream
+        .collect {
+            println("collectt ${Thread.currentThread()}")
+        }
+}
 
-    numFlow().withIndex().collect {
-        println("Values = ${it.index} ${it.value}")
-    }
+private suspend fun foldOperator() {
+    val foldValue = numFlow().take(3).fold("45") { acc: String, value: Int -> acc + value }
+    println("Fold value = $foldValue")
+}
 
-
+private suspend fun reduceOperator() {
     /**
      * reduce operator
      * - Accumulates value starting with the first element and applying operation to current accumulator value and each element
@@ -35,9 +42,27 @@ fun main() = runBlocking {
 
     val reducesValue: Int = numFlow().take(3).reduce { acc, value -> value + acc }
     println("Reduced values = $reducesValue")
+}
 
-    val foldValue = numFlow().take(3).fold("45") { acc: String, value: Int -> acc + value }
-    println("Fold value = $foldValue")
+private suspend fun withIndextedOperator() {
+    numFlow().withIndex().collect {
+        println("Values = ${it.index} ${it.value}")
+    }
+}
+
+
+/**
+ *
+ * Zip operator
+ * https://kotlinlang.org/api/kotlinx.coroutines/kotlinx-coroutines-core/kotlinx.coroutines.flow/zip.html
+ *
+ * Zips values from the current flow (this) with other flow using provided transform function applied to each pair of values.
+ * The resulting flow completes as soon as one of the flows completes and cancel is called on the remaining flow.
+ */
+private suspend fun zipOperator() {
+    numFlow().zip(namesFlow()) { num, name -> "$num - $name" }.collect {
+        println("Zipped values = $it")
+    }
 }
 
 fun numFlow() = flow {
